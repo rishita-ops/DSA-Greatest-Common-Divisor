e0c78bf0d3562a9ac9b1666f093be2b8c35e975e
rishita-ops <rishita.sanghavi07@gmail.com>
2026-03-11 11:33:51 +0530

    First Commit

---
+++ code.cpp
@@ -0,0 +1,23 @@
+//Greates Common Divisor
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b,gcd;
+    cout<<"Enter First Number: ";
+    cin>>a;
+    cout<<"Enter Second Number: ";
+    cin>>b;
+
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+
+    gcd = a;
+
+    cout << "GCD is: " << gcd << endl;
+
+    return 0;
+}
